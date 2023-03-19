03fa01165e1e044be18b3ec15ec8162d5cfdd8b3
anantaa <33261917+anantaabdlh@users.noreply.github.com>
2023-03-19 16:20:11 +0800

    Update bonusblock.sh

--- testnet/bonusblock/bonusblock.sh
+++ testnet/bonusblock/bonusblock.sh
@@ -3,40 +3,39 @@
 #
 
 echo -e "\033[0;31m"
-echo "Script by MapleSyrup"
-echo "        Auto Installer blocktopia-01 {cosmovisor} v1.1.0-beta1       ";
+echo "  Script By MapleSyrup  ";
+echo "        Auto Installer blocktopia-01       ";
 echo -e "\e[0m"
 sleep 1
 
 # Variable
 BONUS_WALLET=wallet
 BONUS=bonus-blockd
-BINARY=cosmovisor
 BONUS_ID=blocktopia-01
 BONUS_FOLDER=.bonusblock
 BONUS_REPO=https://github.com/BBlockLabs/BonusBlock-chain
+BONUS_DENOM=ubonus
 BONUS_GENESIS=https://bonusblock-testnet.alter.network/genesis
 BONUS_ADDRBOOK=https://raw.githubusercontent.com/sxlzptprjkt/resource/master/testnet/bonusblock/addrbook.json
-BONUS_DENOM=ubonus
 BONUS_PORT=18
+
 
 echo "export BONUS_WALLET=${BONUS_WALLET}" >> $HOME/.bash_profile
 echo "export BONUS=${BONUS}" >> $HOME/.bash_profile
-echo "export BINARY=${BINARY}" >> $HOME/.bash_profile
 echo "export BONUS_ID=${BONUS_ID}" >> $HOME/.bash_profile
 echo "export BONUS_FOLDER=${BONUS_FOLDER}" >> $HOME/.bash_profile
 echo "export BONUS_VER=${BONUS_VER}" >> $HOME/.bash_profile
 echo "export BONUS_REPO=${BONUS_REPO}" >> $HOME/.bash_profile
+echo "export BONUS_DENOM=${BONUS_DENOM}" >> $HOME/.bash_profile
 echo "export BONUS_GENESIS=${BONUS_GENESIS}" >> $HOME/.bash_profile
 echo "export BONUS_ADDRBOOK=${BONUS_ADDRBOOK}" >> $HOME/.bash_profile
-echo "export BONUS_DENOM=${BONUS_DENOM}" >> $HOME/.bash_profile
 echo "export BONUS_PORT=${BONUS_PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 # Set Vars
 if [ ! $BONUS_NODENAME ]; then
-	read -p "sxlzptprjkt@w00t666w00t:~# [ENTER YOUR NODE] > " BONUS_NODENAME
-	echo 'export BONUS_NODENAME='$BONUS_NODENAME >> $HOME/.bash_profile
+        read -p "sxlzptprjkt@w00t666w00t:~# [ENTER YOUR NODE] > " BONUS_NODENAME
+        echo 'export BONUS_NODENAME='$BONUS_NODENAME >> $HOME/.bash_profile
 fi
 echo ""
 echo -e "YOUR NODE NAME : \e[1m\e[31m$BONUS_NODENAME\e[0m"
@@ -61,21 +60,13 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 source ~/.bash_profile
 go version
 
-# Get testnet version of bonus-block
+# Get testnet version of BONUS
 cd $HOME
 rm -rf BonusBlock-chain
 git clone $BONUS_REPO
 cd BonusBlock-chain
 make build
-go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-
-# Prepare binaries for Cosmovisor
-mkdir -p $HOME/$BONUS_FOLDER/$BINARY/genesis/bin
-mv build/BonusBlock-chain $HOME/$BONUS_FOLDER/$BINARY/genesis/bin/
-
-# Create application symlinks
-ln -s $HOME/$BONUS_FOLDER/$BINARY/genesis $HOME/$BONUS_FOLDER/$BINARY/current
-sudo ln -s $HOME/$BONUS_FOLDER/$BINARY/current/bin/$BONUS /usr/bin/$BONUS
+sudo mv $HOME/go/bin/$BONUS /usr/bin/
 
 # Init generation
 $BONUS config chain-id $BONUS_ID
@@ -89,7 +80,7 @@ SEEDS="e5e04918240cfe63e20059a8abcbe62f7eb05036@bonusblock-testnet-p2p.alter.net
 sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$BONUS_FOLDER/config/config.toml
 sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$BONUS_FOLDER/config/config.toml
 
-# Download genesis and addrbook
+# Create file genesis.json
 curl -Ls $BONUS_GENESIS > $HOME/$BONUS_FOLDER/config/genesis.json
 curl -Ls $BONUS_ADDRBOOK > $HOME/$BONUS_FOLDER/config/addrbook.json
 
@@ -108,24 +99,21 @@ sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$BONUS_FOLDER/config/app.toml
 
 # Set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0$BONUS_DENOM\"/" $HOME/$BONUS_FOLDER/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025$BONUS_DENOM\"/" $HOME/$BONUS_FOLDER/config/app.toml
 
 
 
 # Create Service
-sudo tee /etc/systemd/system/$BONUS.service > /dev/null << EOF
+sudo tee /etc/systemd/system/$BONUS.service > /dev/null <<EOF
 [Unit]
-Description=$BINARY
+Description=$BONUS
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which $BINARY) run start
+ExecStart=$(which $BONUS) start
 Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-Environment="DAEMON_HOME=$HOME/$BONUS_FOLDER"
-Environment="DAEMON_NAME=$BONUS"
-Environment="UNSAFE_SKIP_BACKUP=true"
+RestartSec=3
+LimitNOFILE=4096
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -137,9 +125,8 @@ sudo systemctl start $BONUS
 
 echo -e "\e[1m\e[31mSETUP FINISHED\e[0m"
 echo ""
-echo -e "CHECK STATUS BINARY : \e[1m\e[31msystemctl status $BONUS\e[0m"
-echo -e "CHECK RUNNING LOGS  : \e[1m\e[31mjournalctl -fu $BONUS -o cat\e[0m"
-echo -e "CHECK LOCAL STATUS  : \e[1m\e[31mcurl -s localhost:${BONUS_PORT}657/status | jq .result.sync_info\e[0m"
+echo -e "CHECK RUNNING LOGS : \e[1m\e[31mjournalctl -fu $BONUS -o cat\e[0m"
+echo -e "CHECK LOCAL STATUS : \e[1m\e[31mcurl -s localhost:${BONUS_PORT}657/status | jq .result.sync_info\e[0m"
 echo ""
 
 # End
