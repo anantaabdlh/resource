32c3a3bc5861607a0634abe5dabc4c835e1b2d79
anantaa <33261917+anantaabdlh@users.noreply.github.com>
2023-03-20 20:52:54 +0800

    Create core.sh

---
+++ mainnet/coreum/core.sh
@@ -0,0 +1,166 @@
+# Variable
+BIN_NAME=cored-linux-arm64
+WALLET=wallet
+BINARY=cored
+CHAIN=coreum-mainnet-1
+FOLDER=.core
+#VERSION=v1.0.0
+COSMOVISOR=cosmovisor
+REPO=https://github.com/CoreumFoundation/coreum/releases/download/v1.0.0/cored-linux-arm64
+GENESIS=https://snap.nodexcapital.com/coreum/genesis.json
+ADDRBOOK=https://snap.nodexcapital.com/coreum/addrbook.json
+DENOM=ucore
+PORT=141
+
+
+# Set Vars
+if [ ! $NODENAME ]; then
+	read -p "hello@nodexcapital:~# [ENTER YOUR NODE] > " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+
+echo "Verify the information below before proceeding with the installation!"
+echo ""
+echo -e "NODE NAME      : \e[1m\e[35m$NODENAME\e[0m"
+echo -e "WALLET NAME    : \e[1m\e[35m$WALLET\e[0m"
+echo -e "CHAIN NAME     : \e[1m\e[35m$CHAIN\e[0m"
+#echo -e "NODE VERSION   : \e[1m\e[35m$VERSION\e[0m"
+echo -e "NODE FOLDER    : \e[1m\e[35m$FOLDER\e[0m"
+echo -e "NODE DENOM     : \e[1m\e[35m$DENOM\e[0m"
+echo -e "NODE ENGINE    : \e[1m\e[35m$COSMOVISOR\e[0m"
+echo -e "SOURCE CODE    : \e[1m\e[35m$REPO\e[0m"
+echo -e "NODE PORT      : \e[1m\e[35m$PORT\e[0m"
+echo ""
+
+read -p "Is the above information correct? (y/n) " choice
+if [[ $choice == [Yy]* ]]; then
+
+echo "export SOURCE=${SOURCE}" >> $HOME/.bash_profile
+echo "export BIN_NAME=${BIN_NAME}" >> $HOME/.bash_profile
+echo "export WALLET=${WALLET}" >> $HOME/.bash_profile
+echo "export BINARY=${BINARY}" >> $HOME/.bash_profile
+echo "export CHAIN=${CHAIN}" >> $HOME/.bash_profile
+echo "export FOLDER=${FOLDER}" >> $HOME/.bash_profile
+echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+#echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
+echo "export REPO=${REPO}" >> $HOME/.bash_profile
+echo "export COSMOVISOR=${COSMOVISOR}" >> $HOME/.bash_profile
+echo "export GENESIS=${GENESIS}" >> $HOME/.bash_profile
+echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+else
+    echo "Installation cancelled!"
+    exit 1
+fi
+sleep 1
+
+# Package
+sudo apt -q update
+sudo apt -qy install curl git jq lz4 build-essential
+sudo apt -qy upgrade
+sudo apt install snapd -y
+sudo snap install lz4
+
+# Install GO
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.19.7.linux-arm64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+# Get mainnet version of planq
+cd $HOME
+curl -LOf $REPO
+chmod +x $BIN_NAME
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+
+# Prepare binaries for Cosmovisor
+mkdir -p $HOME/$FOLDER/$CHAIN/$COSMOVISOR/genesis/bin
+mv $BIN_NAME $HOME/$FOLDER/$CHAIN/$COSMOVISOR/genesis/bin/$BINARY
+rm -rf build
+
+# Create application symlinks
+ln -s $HOME/$FOLDER/$CHAIN/$COSMOVISOR/genesis $HOME/$FOLDER/$CHAIN/$COSMOVISOR/current
+sudo ln -s $HOME/$FOLDER/$CHAIN/$COSMOVISOR/current/bin/$BINARY /usr/local/bin/$BINARY
+
+# Init generation
+$BINARY config chain-id $CHAIN
+$BINARY config keyring-backend file
+$BINARY config node tcp://localhost:${PORT}57
+$BINARY init $NODENAME --chain-id $CHAIN
+
+# Set peers and seeds
+PEERS="8cedd961a72c183686e9b0b67b6e54fccd6471c3@35.194.10.107:26656,81e76bc013acbb2048e7acfb2ab04d80732a3699@34.122.166.246:26656,55cec213e8f3738d2642147d857afab93b1a4ef6@34.172.192.61:26656,62b207017a272a1452ebe7e67018a4f6be1146d8@34.172.201.60:26656,094189cad7921baf44c280ee8efed959869f3a22@34.66.215.21:26656,d65085259afd2065796bba7430d61fe85042e1c3@190.92.219.25:26656,eeb17ff4b1dad8d20fdafc339c277f7a624bb84a@35.238.253.76:26656,92b67a34dbda739a92cd04561ac8c33bfa858477@34.67.59.88:26656,2505072cc9586c0c4fafa092a2352123d8c12936@34.28.225.76:26656"
+SEEDS=""
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$FOLDER/$CHAIN/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$FOLDER/$CHAIN/config/config.toml
+
+# Download genesis and addrbook
+curl -Ls $GENESIS > $HOME/$FOLDER/$CHAIN/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$FOLDER/$CHAIN/config/addrbook.json
+
+# Set Port
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}58\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}57\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}60\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}56\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}60\"%" $HOME/$FOLDER/$CHAIN/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}17\"%; s%^address = \":8080\"%address = \":${PORT}80\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}90\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}91\"%" $HOME/$FOLDER/$CHAIN/config/app.toml
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="19"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+
+# Set Config prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/$FOLDER/$CHAIN/config/config.toml
+
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0$DENOM\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+
+# Enable State Sync
+sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = \"2000\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+$BINARY tendermint unsafe-reset-all --home $HOME/$FOLDER/$CHAIN --keep-addr-book
+curl -L https://snap.nodexcapital.com/coreum/coreum-latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/$FOLDER/$CHAIN
+
+#Delete Trash File
+cd $HOME/$FOLDER/$CHAIN/
+rm -rf $CHAIN
+rm -rf .coreum
+
+# Create Service
+sudo tee /etc/systemd/system/$BINARY.service > /dev/null << EOF
+[Unit]
+Description=$BINARY
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/$FOLDER/$CHAIN"
+Environment="DAEMON_NAME=$BINARY"
+Environment="UNSAFE_SKIP_BACKUP=true"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Register And Start Service
+sudo systemctl daemon-reload
+sudo systemctl enable $BINARY
+sudo systemctl start $BINARY
+
+echo -e "\033[0;35m=============================================================\033[0m"
+echo -e "\033[0;35mCONGRATS! SETUP FINISHED\033[0m"
+echo ""
+echo -e "CHECK STATUS BINARY : \033[1m\033[35msystemctl status $BINARY\033[0m"
+echo -e "CHECK RUNNING LOGS : \033[1m\033[35mjournalctl -fu $BINARY -o cat\033[0m"
+echo -e "CHECK LOCAL STATUS : \033[1m\033[35mcurl -s localhost:${PORT}57/status | jq .result.sync_info\033[0m"
+echo -e "\033[0;35m=============================================================\033[0m"
+
+# End
